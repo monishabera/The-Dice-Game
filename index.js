c240e1680a3f6b25d8031f7e912a063aba06a661
monishabera <120238238+monishabera@users.noreply.github.com>
2023-03-14 12:12:54 +0530

    spelling mistake

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // for 1st dice
 var randomNumber1 = Math.floor(Math.random()*6)+1; //1-6
 var randomDiceImage = "dice"+randomNumber1+".png"; //dice1.png-dice6.png
-var randomImageSource = "Images/"+randomDiceImage; //images/dice1.png-images/dice6.png
+var randomImageSource = "images/"+randomDiceImage; //images/dice1.png-images/dice6.png
 var image1 = document.querySelectorAll("img")[0];
 image1.setAttribute("src", randomImageSource);
 
